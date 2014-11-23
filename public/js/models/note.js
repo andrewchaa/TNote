dee41fa3b132a036eb0547579e5a4afe263436a5
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-11-23 19:03:46 +0000

    update works

--- public/js/models/note.js
+++ public/js/models/note.js
@@ -3,21 +3,42 @@ var Note = Base.extend({
 		that = this;
 		that.title = ko.observable(title);
 		that.content = ko.observable(content);
-		that.links = ko.observableArray();
+		that.selfLink = ko.observable('');
+		that.updateLink = ko.observable('');
+		that.deleteLink = ko.observable('');
+		that.saveBtnStatus = ko.computed(function () {
+			return that.updateLink() !== '' ? 'btn btn-primary' : 'btn btn-default'; 
+		});
 	}, 
 
 	add: function (note) {
 		$.post('/api/notes', { title: note.title, note: note.content })
 			.done(function (response) { 
 				response.links.map(function (link) {
-					that.links.push(link);
+					if (link.rel === 'self') { 
+						that.selfLink(link.link);
+					} else if (link.rel === 'update') {
+						that.updateLink(link.link);
+					} else if (link.rel === 'delete') {
+						that.deleteLink(link.link);
+					}
+					console.log(link);
 				});
 			})
 			.fail(function (error) { console.log(error); });
 	},
 
 	update: function (note) {
-		$.post('/api/notes/');
+		console.log(note);
+		if (note.updateLink()) {
+			$.ajax({
+				type: 'PUT',
+				url: note.updateLink(), 
+				data: { title: note.title(), note: note.content() }
+			})
+			.done(function (response) {})
+			.fail(function (error) { console.log(error); });	
+		}
 	},
 
 	list: function () {
