f3a9d435fa54dcae659b542d4cdcc1e26fab4f84
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-12-15 23:47:54 +0000

    factory to remove duplication

--- public/js/controller.js
+++ public/js/controller.js
@@ -15,67 +15,56 @@ noteApp = angular.module('noteApp', ['ngRoute'])
     }
   ])
 
-  .controller('homeCtrl', ['$scope', '$http', '$location', function noteCtrl($scope, $http, $location) {
-
-    $http.get('/api/notes')
-      .success(function (data, status, headers, config) {
-        $scope.notes = data;
-    })
-
-
-    CKEDITOR.disableAutoInline = true;
-
-    var config = {
-      extraPlugins: 'codesnippet',
-      toolbar: [
-        [ 'Source' ], [ 'Undo', 'Redo' ], [ 'Bold', 'Italic', 'Underline' ], [ 'CodeSnippet' ]
-      ],
-      codeSnippet_theme: 'default',
-      height: 400
-    };
-
-    CKEDITOR.inline( 'content', CKEDITOR.tools.extend( {}, config, {
-      extraPlugins: 'codesnippet'
-    }, true ) );
-
-    if ( CKEDITOR.env.ie && CKEDITOR.env.version == 8 ) {
-      CKEDITOR.document.getById( 'ie8-warning' ).addClass( 'warning' );
+  .factory('listNotes', ['$http', function ($http) {
+    return function (scope) {
+      $http.get('/api/notes')
+        .success(function (notes, status, headers, config) {
+          scope.notes = notes;
+      });      
     }
-
-    $scope.save = function(e) {
-      var title = $scope.title;
-      var content = CKEDITOR.instances.content.getData();
-
-      $http.post('/api/notes', { title: title, note: content })
-        .success(function (note, status, headers, config) {
-          $location.path('/' + note.id);
-        });
-    }
-
-
   }])
 
-  .controller('editCtrl', ['$scope', '$routeParams', '$http', '$location', 
-    function ($scope, $routeParams, $http, $location) {
-
+  .factory('bindEditor', [function () {
+    return function (elementId) {
       CKEDITOR.disableAutoInline = true;
 
       var config = {
         extraPlugins: 'codesnippet',
-        toolbar: [
-          [ 'Source' ], [ 'Undo', 'Redo' ], [ 'Bold', 'Italic', 'Underline' ], [ 'CodeSnippet' ]
-        ],
+        toolbar: [[ 'Source' ], [ 'Undo', 'Redo' ], [ 'Bold', 'Italic', 'Underline' ], [ 'CodeSnippet' ]],
         codeSnippet_theme: 'default',
         height: 400
       };
 
-      CKEDITOR.inline( 'content', CKEDITOR.tools.extend( {}, config, {
-        extraPlugins: 'codesnippet'
-      }, true ) );
+      CKEDITOR.inline( elementId, CKEDITOR.tools.extend( {}, config, {extraPlugins: 'codesnippet'}, true ) );
 
       if ( CKEDITOR.env.ie && CKEDITOR.env.version == 8 ) {
         CKEDITOR.document.getById( 'ie8-warning' ).addClass( 'warning' );
       }
+
+    }
+  }])
+
+  .controller('homeCtrl', ['$scope', '$http', '$location', 'listNotes', 'bindEditor',
+    function noteCtrl($scope, $http, $location, listNotes, bindEditor) {
+
+      listNotes($scope);
+      bindEditor('content');
+
+      $scope.save = function(e) {
+        var title = $scope.title;
+        var content = CKEDITOR.instances.content.getData();
+
+        $http.post('/api/notes', { title: title, note: content })
+          .success(function (note, status, headers, config) {
+            $location.path('/' + note.id);
+          });
+      }
+
+
+  }])
+
+  .controller('editCtrl', ['$scope', '$routeParams', '$http', '$location', 'listNotes', 'bindEditor',
+    function ($scope, $routeParams, $http, $location, listNotes, bindEditor) {
 
       $http.get('/api/notes/' + $routeParams.id)
         .success(function (note, status, headers, config) {
