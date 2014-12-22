0e44b41270d44dd5ffe9034ad008eb2307973d50
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-12-22 22:56:39 +0000

    use facebook login

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,19 +1,46 @@
 (function (controller) {
+
   controller.init = function (app, passport) {
+    var config = require('../../oauth.js'),
+        mongoose = require('mongoose'),
+        passport = require('passport'),
+        FacebookStrategy = require('passport-facebook').Strategy;
+
+    passport.serializeUser(function (user, next) {
+      next(null, user);
+    });
+
+    passport.deserializeUser(function (obj, next) {
+      next(null, obj);
+    })
+
+    passport.use(new FacebookStrategy({
+      clientID: config.facebook.clientID,
+      clientSecret: config.facebook.clientSecret,
+      callbackURL: config.facebook.callbackURL
+    }, function (accessToken, refreshToken, profile, next) {
+      console.log('accessToken: ' + accessToken);
+      console.log('id: ' + profile.id);
+      process.nextTick(function () {
+        next(null, profile);  
+      })      
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
     app.get("/", function (req, res) {
         res.render("index", {});
     });
 
-    app.post(
-      '/login', 
-      passport.authenticate('local', { 
-        successRedirect: '/', 
-        failureRedirect: '/login',
-        successFlash: 'Welcome!',
-        failureFlash: true
-      }), 
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+    app.get('/auth/facebook/callback', 
+      passport.authenticate('facebook', { failureRedirect: "/#/loginfailed" }),
       function (req, res) {
-      
+        console.log('login success');
+        res.redirect('/#');
     });
+
+
   };
 })(module.exports);
